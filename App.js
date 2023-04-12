6d79077afd5dee3cbf599c2c50d184edbf3f887c
disavey <118791495+disavey@users.noreply.github.com>
2023-04-12 10:57:19 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import MyButton from "./component/MyButton";
+
+function App() {
+  return (
+    <div>
+      <h1 id="up">Maaf pak saya lupa gimana caranya, maaf banget pak</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ MyButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyButton = () => {
+  return (
+    <div>
+        <button>'Upper case'</button>
+    </div>
+  )
+}
+
+export default MyButton
